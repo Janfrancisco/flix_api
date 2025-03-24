a417ffd8464cf6c759dffd87b374c8d677e5f4f9
Janfrancisco <janramalho.100@gmail.com>
2025-03-23 21:50:38 -0400

    feat:add README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+<h1 style="text-align:center; font-weight:bold;">Flix API</h1>
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+![Django](https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white)
+## Table of Contents:
+
+- [About](#about)
+- [Instalation](#Instalation)
+- [Usage](#usage)
+
+### About
+
+REST API that handles information about Movies, containing cast of actors and a review section
+
+### Installation
+
+### Usage
+
+### API Endpoints
+
+### Configuration
+
+### Contributing
+
+### License
+
+### Acknowledgments
--- genres/views.py
+++ genres/views.py
@@ -16,46 +16,4 @@ class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-
-
-#@csrf_exempt
-#def genre_create_list_view(request):
-#    if request.method == 'GET':
-#        genres = Genre.objects.all()
-#        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-#        return JsonResponse(
-#            data,
-#            safe=False
-#        )
-#    
-#    elif request.method == 'POST':
-#        data = json.loads(request.body.decode('utf-8'))
-#        new_genre = Genre(name=data['name'])
-#        new_genre.save()
-#        return JsonResponse(
-#            {'id': new_genre.id, 'name': new_genre.name},
-#            status=201
-#        )
-
-# @csrf_exempt   
-# def genre_detail_view(request, pk):
-#   genre = get_object_or_404(Genre, pk=pk)
-#
-#   if request.method == 'GET':
-#       return JsonResponse(
-#           {'id': genre.id, 'name': genre.name}
-#       )
-#   elif request.method == 'PUT':
-#        data = json.loads(request.body.decode('utf-8'))
-#        #updated_genre = Genre.objects.filter(pk=pk).update(name=data['name'])
-#        genre.name = data['name']
-#        genre.save()
-#        return JsonResponse(
-#            {'id': genre.id, 'name': data['name']}
-#        )  
-#   elif request.method == 'DELETE':
-#       genre.delete()     
-#       return JsonResponse(
-#           {'message': 'Genre deleted successfully'}, 
-#           status=204
-#       ) 
+ 
